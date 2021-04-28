408993e7421d38387d74ce99258498325b996ae4
zhangpark <787326576@qq.com>
2021-04-28 09:27:51 +0800

    删除错误文件

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,20 +1,17 @@
 <template>
   <div id="app">
-    <!-- <landing-page></landing-page> -->
     <c-header></c-header>
     <basic-layout></basic-layout>
   </div>
 </template>
 
 <script>
-  import LandingPage from '@/components/LandingPage'
   import BasicLayout from '@/components/BasicLayout'
   import CHeader from '@/components/title/titleIndex'
 
   export default {
     name: 'myelectron',
     components: {
-      LandingPage,
       BasicLayout,
       CHeader
     }
